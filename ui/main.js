2f7ffa051412a883f27a4fd432309c430a674435
amulyasai <amulyasaiinturi@gmail.com>
2016-10-04 22:20:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,14 @@ var element=document.getElementById('yo');
 element.innerHTML='COFFEE!!!';
 
 var img = document.getElementById('madi');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
+
 img.onclick= function() {
+    var interval=setInterval(moveRight,100);
     img.style.marginLeft='100px';
 };
